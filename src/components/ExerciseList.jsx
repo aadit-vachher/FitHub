76e516baad0b8b99aa90c05192075b4d50c539a7
Aadit Vachher <vacaadit@gmail.com>
2025-04-24 00:20:09 +0530

    exercises added

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -1,3 +1,23 @@
+// import React from 'react';
+// import './ExerciseList.css';
+
+// const ExerciseList = ({ muscle, exercises }) => {
+//   return (
+//     <div className="exercise-list">
+//       <h3>{muscle} Exercises</h3>
+//       <ul>
+//         {exercises[muscle].map((exercise, index) => (
+//           <li key={index}>{exercise}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ExerciseList;
+
+
+
 import React from 'react';
 import './ExerciseList.css';
 
@@ -7,7 +27,20 @@ const ExerciseList = ({ muscle, exercises }) => {
       <h3>{muscle} Exercises</h3>
       <ul>
         {exercises[muscle].map((exercise, index) => (
-          <li key={index}>{exercise}</li>
+          <li key={index} className="exercise-item">
+            <p className="exercise-name">{exercise.name}</p>
+            <div className="video-wrapper">
+              <iframe
+                width="100%"
+                height="315"
+                src={exercise.videoUrl}
+                title={exercise.name}
+                // frameBorder="5"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </li>
         ))}
       </ul>
     </div>
--- src/components/MuscleMap.jsx
+++ src/components/MuscleMap.jsx
@@ -6,12 +6,51 @@ import ExerciseList from './ExerciseList';
 const MuscleMap = () => {
   const [selectedMuscle, setSelectedMuscle] = useState(null);
   const exercises = {
-    Biceps: ['Barbell Curl', 'Hammer Curl', 'Concentration Curl'],
-    Triceps: ['Tricep Dips', 'Overhead Tricep Extension', 'Tricep Pushdown'],
-    Chest: ['Bench Press', 'Push-ups', 'Incline Dumbbell Press'],
-    Back: ['Deadlifts', 'Pull-ups', 'Bent Over Rows'],
-    Legs: ['Squats', 'Lunges', 'Leg Press'],
+    Biceps: [
+      { name: 'Barbell Curl', videoUrl: 'https://www.youtube.com/embed/kwG2ipFRgfo' },
+      { name: 'Hammer Curl', videoUrl: 'https://www.youtube.com/embed/zC3nLlEvin4' },
+      { name: 'Concentration Curl', videoUrl: 'https://www.youtube.com/embed/av7-8igSXTs' }
+    ],
+    Triceps: [
+      { name: 'Tricep Dips', videoUrl: 'https://www.youtube.com/embed/6kALZikXxLc' },
+      { name: 'Overhead Tricep Extension', videoUrl: 'https://www.youtube.com/embed/YbX7Wd8jQ-Q' },
+      { name: 'Tricep Pushdown', videoUrl: 'https://www.youtube.com/embed/2-LAMcpzODU' }
+    ],
+    Chest: [
+      { name: 'Bench Press', videoUrl: 'https://www.youtube.com/embed/gRVjAtPip0Y' },
+      { name: 'Push-ups', videoUrl: 'https://www.youtube.com/embed/_l3ySVKYVJ8' },
+      { name: 'Incline Dumbbell Press', videoUrl: 'https://www.youtube.com/embed/8iPEnn-ltC8' }
+    ],
+    Back: [
+      { name: 'Deadlifts', videoUrl: 'https://www.youtube.com/embed/op9kVnSso6Q' },
+      { name: 'Pull-ups', videoUrl: 'https://www.youtube.com/embed/eGo4IYlbE5g' },
+      { name: 'Bent Over Rows', videoUrl: 'https://www.youtube.com/embed/vT2GjY_Umpw' }
+    ],
+    Legs: [
+      { name: 'Squats', videoUrl: 'https://www.youtube.com/embed/aclHkVaku9U' },
+      { name: 'Lunges', videoUrl: 'https://www.youtube.com/embed/QOVaHwm-Q6U' },
+      { name: 'Leg Press', videoUrl: 'https://www.youtube.com/embed/IZxyjW7MPJQ' }
+    ],
+    Shoulders: [
+      { name: 'Shoulder Press', videoUrl: 'https://www.youtube.com/embed/B-aVuyhvLHU' },
+      { name: 'Lateral Raise', videoUrl: 'https://www.youtube.com/embed/3VcKaXpzqRo' },
+      { name: 'Front Raise', videoUrl: 'https://www.youtube.com/embed/-t7fuZ0KhDA' }
+    ],
+    Calves: [
+      { name: 'Standing Calf Raise', videoUrl: 'https://www.youtube.com/embed/-M4-G8p8fmc' },
+    ],
+    Abs: [
+      { name: 'Crunches', videoUrl: 'https://www.youtube.com/embed/Xyd_fa5zoEU' },
+      { name: 'Leg Raises', videoUrl: 'https://www.youtube.com/embed/JB2oyawG9KI' },
+      { name: 'Plank', videoUrl: 'https://www.youtube.com/embed/pSHjTRCQxIw' }
+    ],
+    Glutes: [
+      { name: 'Hip Thrusts', videoUrl: 'https://www.youtube.com/embed/SEdqd1n0cvg' },
+    ],
   };
+  
+  
+  
   
 
   const handleMuscleClick = (muscle) => {
@@ -29,7 +68,12 @@ const MuscleMap = () => {
         <button className="sub-muscle-button" onClick={() => handleMuscleClick('Chest')}>Chest</button>
         <button className="sub-muscle-button" onClick={() => handleMuscleClick('Back')}>Back</button>
         <button className="sub-muscle-button" onClick={() => handleMuscleClick('Legs')}>Legs</button>
+        <button className="sub-muscle-button" onClick={() => handleMuscleClick('Shoulders')}>Shoulders</button>
+        <button className="sub-muscle-button" onClick={() => handleMuscleClick('Calves')}>Calves</button>
+        <button className="sub-muscle-button" onClick={() => handleMuscleClick('Abs')}>Abs</button>
+        <button className="sub-muscle-button" onClick={() => handleMuscleClick('Glutes')}>Glutes</button>
       </div>
+
 
 
       {selectedMuscle && (
